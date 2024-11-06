10e34ff520182e9bc3e3ecec9f3115ccf7f7302b
Elias <eliasnau09@gmail.com>
2024-11-06 17:57:50 +0100

    feat: add /users endpoint

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import authenticationMiddleware from "../../middleware/authMiddleware";
 import auth from "./auth";
+import users from "./users";
 
 const router = express.Router();
 
 router.use("/auth", auth);
+router.use("/users", users);
 
 export default router;
