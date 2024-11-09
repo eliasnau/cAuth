3befd982f9d6dd6ff681571aba90d65892813678
Elias Nau <elias.nau@outlook.de>
2024-11-09 13:23:14 +0100

    chore: updated setup-server to not install docker

--- setup-server.sh
+++ setup-server.sh
@@ -11,13 +11,6 @@ sudo apt install -y \
     software-properties-common \
     git
 
-# Install Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 
 # Create app directory
 sudo mkdir -p /opt/auth
